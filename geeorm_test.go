5d83d43dbaff76905fd0f5ca8ae5ef30e6c4020e
accle <3196385390@qq.com>
2024-10-25 17:12:45 +0800

    feat: migrate

--- geeorm_test.go
+++ geeorm_test.go
@@ -3,6 +3,7 @@ package geeorm
 import (
 	"errors"
 	"geeorm/session"
+	"reflect"
 	"testing"
 )
 
@@ -40,7 +41,7 @@ func transactionRollback(t *testing.T) {
 
 	_, err := engine.Transaction(func(s *session.Session) (result interface{}, err error) {
 		_ = s.Model(&User{}).CreateTable()
-		_, err = s.Insert(&User{"Tom", 18})
+		_, _ = s.Insert(&User{"Tom", 18})
 		return nil, errors.New("Error")
 	})
 
@@ -65,5 +66,22 @@ func transactionCommit(t *testing.T) {
 	s.First(u)
 	if err != nil || u.Name != "Tom" {
 		t.Fatal("failed to commit")
+	}
+}
+
+
+func TestEngine_Migrate(t *testing.T) {
+	engine := OpenDB(t)
+	defer engine.Close()
+	s := engine.NewSession()
+	_, _ = s.Raw("DROP TABLE IF EXISTS User;").Exec()
+	_, _ = s.Raw("CREATE TABLE User(Name text PRIMARY KEY, XXX integer);").Exec()
+	_, _ = s.Raw("INSERT INTO User(`Name`) values (?), (?)", "Tom", "Sam").Exec()
+	engine.Migrate(&User{})
+
+	rows, _ := s.Raw("SELECT * FROM User").QueryRows()
+	columns, _ := rows.Columns()
+	if !reflect.DeepEqual(columns, []string{"Name", "Age"}) {
+		t.Fatal("Failed to migrate table User, got columns", columns)
 	}
 }
